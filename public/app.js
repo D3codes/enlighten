5b39158ac40ad84645dc6fe82deacf2067771cc7
Caullen Sasnett <caullens@ksu.edu>
2018-02-10 15:24:54 -0600

    Implemented popup windows and tabs

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 let datamap
 let spreadSpeed = 15
 let blessings = 0
+let startState
 let totalPopulation = 323145790
 let totalConverts = 0
 let game_state = 'start'
@@ -19,29 +20,29 @@ let methods = [
     probSpread: 0.0001
   },
   {
-    method: "Radio",
-    probOccur: 0.1,
-    probSpread: 0.01
-  },
-  {
     method: "Televangelism",
     probOccur: 0,
     probSpread: 0.01
   },
   {
-    method: "Print",
-    probOccur: 0.1,
-    probSpread: 0.01
-  },
-  {
-    method: "Congregation",
+    method: "Radio",
     probOccur: 0,
     probSpread: 0.01
   },
   {
     method: "Door-to-Door",
-    probOccur: 0.2,
+    probOccur: 0,
     probSpread: 0.005
+  },
+  {
+    method: "Print",
+    probOccur: 0,
+    probSpread: 0.01
+  },
+  {
+    method: "Congregation",
+    probOccur: 0.1,
+    probSpread: 0.01
   }
 ]
 
@@ -64,7 +65,7 @@ function setup() {
 
   this.canvas = createCanvas(window.innerWidth, window.innerHeight)
   this.canvas.id('canvas')
-  this.popup = createGraphics(500, 500)
+  this.popup = createGraphics(800, 500)
   this.infoBar = createGraphics(window.innerWidth, canvasHeight)
 
   fetch('states.json').then((res) => {
@@ -114,9 +115,9 @@ function render() {
   datamap.updateChoropleth(loc)
 
 
-  if(game_state === 'heathens' || game_state === 'religion' || game_state === 'popup') {
+  if(game_state === 'heathens' || game_state === 'religion' || game_state === 'evangelism' || game_state === 'resistances') {
     this.canvas.style('z-index', '20')
-    image(this.popup, window.innerWidth/2-250, 200)
+    image(this.popup, window.innerWidth/2-this.popup.width/2, 200)
   } else {
     this.canvas.style('z-index', '5')
   }
@@ -125,25 +126,83 @@ function render() {
 
 function updateInfoBar() {
   this.infoBar.background(255, 255, 255)
+
+  this.infoBar.rectMode(RADIUS)
+  this.infoBar.noStroke()
+  this.infoBar.fill(200, 200, 0)
+  this.infoBar.rect(window.innerWidth/5, 40, 200, 50)
+  this.infoBar.fill(255, 50, 50)
+  this.infoBar.rect(4*window.innerWidth/5, 40, 200, 50)
+
   this.infoBar.fill(0, 0, 0)
+  this.infoBar.textFont('IM Fell English SC')
+  this.infoBar.textSize(30)
   this.infoBar.textAlign(CENTER, CENTER)
   this.infoBar.text('Religion', window.innerWidth/5, 40)
   this.infoBar.text(`Total Enlightened: ${totalConverts}`, window.innerWidth/2, 30)
-  this.infoBar.text(`Blessings: ${blessings}`, window.innerWidth/2, 50)
+  this.infoBar.text(`Blessings: ${blessings}`, window.innerWidth/2, 60)
   this.infoBar.text('Heathens', 4*window.innerWidth/5, 40)
 }
 
 function updatePopup() {
-  this.popup.background(51)
+  this.popup.textAlign(LEFT)
+  this.popup.textFont('IM Fell English SC')
+  switch(game_state) {
+    case 'religion':
+      this.popup.background(20, 160, 50)
+      this.popup.fill(0)
+      this.popup.textSize(50)
+      this.popup.text('Religion', 50, 50)
+
+      this.popup.textSize(30)
+      this.popup.text(`Total Population: ${totalPopulation}`, 50, 100)
+      this.popup.text(`Start State: ${startState}`, 50, 150)
+      break;
+    case 'evangelism':
+      this.popup.background(220)
+
+      break;
+    case 'resistances':
+      this.popup.background(50, 50, 200)
+
+      break;
+  }
+
+  this.popup.noStroke()
+  this.popup.fill(20, 160, 50)
+  this.popup.rect(0, popup.height-50, popup.width/3, 50)
+  this.popup.fill(220)
+  this.popup.rect(popup.width/3, popup.height-50, popup.width/3, 50)
+  this.popup.fill(50, 50, 200)
+  this.popup.rect(2*popup.width/3, popup.height-50, popup.width/3, 50)
+  this.popup.fill(0)
+  this.popup.textAlign(CENTER, CENTER)
+  this.popup.text("Religion", this.popup.width/2 - this.popup.width/3, this.popup.height - 25)
+  this.popup.text("Evangelism", this.popup.width/2, this.popup.height - 25)
+  this.popup.text("Resistances", this.popup.width/2 + this.popup.width/3, this.popup.height - 25)
 }
 
 function mouseClicked() {
-  if(mouseY < window.innerHeight && mouseY > window.innerHeight - canvasHeight) {
-    if(mouseX < window.innerWidth && mouseX > window.innerWidth - 200) {
+  if(mouseY < window.innerHeight && mouseY > window.innerHeight - 100) {
+    if(mouseX < 4*window.innerWidth/5 + 200 && mouseX > 4*window.innerWidth/5 - 200) {
       game_state = 'heathens'
     }
-    if(mouseX < 200 && mouseX > 0) {
+    if(mouseX < window.innerWidth/5 + 200 && mouseX > window.innerWidth/5 - 200) {
       game_state = 'religion'
+    }
+  }
+
+  if(game_state === 'religion' || game_state === 'evangelism' || game_state === 'resistances') {
+    if(mouseY < window.innerHeight/2+this.popup.height/2-50 && mouseY > window.innerHeight/2+this.popup.height/2-100) {
+      if(mouseX < window.innerWidth/2+this.popup.width/2 && mouseX > window.innerWidth/2+this.popup.width/2 - this.popup.width/3) {
+        game_state = 'resistances'
+      }
+      else if(mouseX < window.innerWidth/2+this.popup.width/2 && mouseX > window.innerWidth/2+this.popup.width/2 - 2*this.popup.width/3) {
+        game_state = 'evangelism'
+      }
+      else if(mouseX < window.innerWidth/2+this.popup.width/2 && mouseX > window.innerWidth/2-this.popup.width/2) {
+        game_state = 'religion'
+      }
     }
   }
 }
@@ -152,6 +211,7 @@ function mapClicked(geography) {
   if(game_state !== 'start') return
   for(let i = 0; i < states.length; i++) {
     if(states[i].sid === geography.id) {
+      startState = states[i].name
       states[i].converted = 1
       game_state = 'play'
     }
--- public/state.js
+++ public/state.js
@@ -11,14 +11,15 @@ class State {
   spread(state, methods) {
     if(this.converted === this.population) return
     if(state.converted === 0) return
-    
-    let loopNum = 2
+
+    let loopNum = 3
     let people = 0
     if(this.sid === state.sid) loopNum = methods.length
-    else if(this.borders.indexOf(state.sid) > -1) loopNum = 4
+    else if(this.borders.indexOf(state.sid) > -1) loopNum = 5
     for(let i = 0; i < loopNum; i++) {
       if(random() < methods[i].probOccur) {
-        people += ceil(random(map(this.population-this.converted, 0, this.population, this.population-this.converted, 1)))
+        people += ceil(random(map(this.converted / methods[i].probSpread, 0, this.population / methods[i].probSpread, 1, this.population - this.converted)))
+        //people += ceil(random(map(this.population-this.converted, 0, this.population, this.population-this.converted, 1)) * methods[i].probSpread)
       }
     }
     this.converted += people
