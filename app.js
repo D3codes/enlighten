41d25f86740d26c165180d112efc7d1ffae68657
Caullen Sasnett <caullens@ksu.edu>
2018-02-09 19:46:39 -0600

    Made map scale by screen size

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight)
+  noCanvas()
+  let container = createDiv('')
+  container.id('container')
+  container.style('width', `${window.innerWidth}`)
+  container.style('height', `${window.innerHeight}`)
+  var datamap = new Datamap({element: document.getElementById('container')});
 }
 
 function draw() {
-  background(0)
 }
